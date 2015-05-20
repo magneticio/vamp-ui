e1d48563925e27b4ed8e99b0ed5a24ca42fe7010
Daniel Sneijers <daniel.sneijers@gmail.com>
2015-05-20 17:15:02 +0200

    added sass/compass config file

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "./build"
+sass_dir = "./src/scss"
+images_dir = "src/www/images"
+javascripts_dir = "./src/app"
+
+
+generated_images_dir = "src/www/images/"
+http_generated_images_path = "images/"
