264ee1883dc6e7ca00a4dea30fe076a0c2ac0150
Daniel Sneijers <daniel.sneijers@gmail.com>
2015-05-29 12:56:18 +0200

    added deployment/blueprints seeds, updated readme

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ You need to have NPM installed. Then...
     $ npm install
     $ npm start
 
+## Seeds
+
+To get you up to speed there's some seeds included, navigate to the assets > seeds folder and execute 
+	
+	$ sh seed.sh
+
+This will add a handful of breeds, blueprints and deployments to VAMP.
+
 ## Tips
 
 during development, start Chrome with disables web security to allow CORS requests.
--- assets/seeds/seed.sh
+++ assets/seeds/seed.sh
@@ -1,14 +1,44 @@
 #! /bin/sh
 BASEDIR=$(dirname $0)
 
-for f in "$BASEDIR"/*; do
+# BREEDS
+for f in "$BASEDIR"/breeds/*; do
 	if [[ $f == *.json ]] 
 	then
-		filename=$(basename "$f")
+		filename=breeds/$(basename "$f")
 		http POST http://192.168.59.103:8080/api/v1/breeds Content-Type:application/json < $filename
 	fi
+	if [[ $f == *.yml ]] 
+	then
+		filename=breeds/$(basename "$f")
+		http POST http://192.168.59.103:8080/api/v1/breeds Content-Type:application/x-yaml < $filename
+	fi
 done
-#cd /my_folder \
-#&& rm *.jar \
-#&& svn co path to repo \
-#&& mvn compile package install
+
+# BLUEPRINTS
+for f in "$BASEDIR"/blueprints/*; do
+	if [[ $f == *.json ]] 
+	then
+		filename=blueprints/$(basename "$f")
+		http POST http://192.168.59.103:8080/api/v1/blueprints Content-Type:application/json < $filename
+	fi
+	if [[ $f == *.yml ]] 
+	then
+		filename=blueprints/$(basename "$f")
+		http POST http://192.168.59.103:8080/api/v1/blueprints Content-Type:application/x-yaml < $filename
+	fi
+done
+
+# DEPLOYMENTS
+for f in "$BASEDIR"/deployments/*; do
+	if [[ $f == *.json ]] 
+	then
+		filename=deployments/$(basename "$f")
+		http POST http://192.168.59.103:8080/api/v1/deployments Content-Type:application/json < $filename
+	fi
+	if [[ $f == *.yml ]] 
+	then
+		filename=deployments/$(basename "$f")
+		http POST http://192.168.59.103:8080/api/v1/deployments Content-Type:application/x-yaml < $filename
+	fi
+done
