edd234df1d44825adfbed7ad1fc20332cc5d6c73
Daniel Sneijers <daniel.sneijers@gmail.com>
2015-07-21 11:00:34 +0200

    bumped timeout to 5 seconds

--- src/app/actions/Api.js
+++ src/app/actions/Api.js
@@ -3,7 +3,7 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var LoadStates = require("../constants/LoadStates.js");
 var request = require('superagent');
 
-var TIMEOUT = 2000;
+var TIMEOUT = 5000;
 var _pendingRequests = {};
 
 // Helpers
