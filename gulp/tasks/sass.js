040407d843b314fa61695c7e198e53e87802946c
Daniel Sneijers <daniel.sneijers@gmail.com>
2015-08-10 17:36:18 +0200

    added sourcemaps for easier css debugging

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,35 +1,60 @@
 var gulp = require('gulp'),
-  compass = require('gulp-compass'),
+  sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
   handleErrors = require('../util/handleErrors'),
   config = require('../config').sass;
 
+
 gulp.task('sass', function() {
-  return gulp.src(config.src)
-    .pipe(sourcemaps.init())
-    .pipe(compass({
-      config_file: './config.rb',
-      css: './build',
-      sass: './src/scss',
-      style: 'expanded'
-    }))
-    .on('error', handleErrors)
-    .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
-    .pipe(sourcemaps.write())
+   return gulp.src(config.src)
+     .pipe(sourcemaps.init())
+     .pipe(sass())
+     .on('error', handleErrors)
+     .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
+     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest));
+
 });
 
 gulp.task('sass-min', function() {
-  return gulp.src(config.src)
-    .pipe(sourcemaps.init())
-    .pipe(compass({
-      config_file: './config.rb',
-      css: './build',
-      sass: './src/scss',
-      style: 'compressed'
-    }))
-    .on('error', handleErrors)
-    .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
+   return gulp.src(config.src)
+     .pipe(sass({outputStyle: 'compressed'}))
+     .on('error', handleErrors)
+     .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
     .pipe(gulp.dest(config.dest));
 });
+
+// gulp.src('./scss/*.scss')
+//   .pipe(sourcemaps.init())
+//     .pipe(sass())
+//   .pipe(sourcemaps.write())
+//   .pipe(gulp.dest('./css'));
+
+// gulp.task('sass', function() {
+//   return gulp.src(config.src)
+//     .pipe(sourcemaps.init())
+//     .pipe(compass({
+//       config_file: './config.rb',
+//       css: './build',
+//       sass: './src/scss',
+//       style: 'expanded'
+//     }))
+//     .on('error', handleErrors)
+//     .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest(config.dest));
+// });
+
+// gulp.task('sass-min', function() {
+//   return gulp.src(config.src)
+//     .pipe(compass({
+//       config_file: './config.rb',
+//       css: './build',
+//       sass: './src/scss',
+//       style: 'compressed'
+//     }))
+//     .on('error', handleErrors)
+//     .pipe(autoprefixer({cascade: false, browsers: ['last 2 versions']}))
+//     .pipe(gulp.dest(config.dest));
+// });
