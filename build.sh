f14c360b6e0e4e3e503f762023e0478cbceda802
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-11-17 00:36:14 +0100

    adding simple build script.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+target="dist"
+
+if [ -n "$1" ]; then
+  target=$1
+fi
+
+cd ${dir}
+
+reset=`tput sgr0`
+green=`tput setaf 2`
+
+echo "${green}Vamp UI build: starting.${reset}"
+echo "${green}Target directory: ${dir}/${target}${reset}"
+
+echo "${green}Cleaning...${reset}"
+rm -Rf ${dir}/build ${dir}/${target}
+
+echo "${green}Running npm install...${reset}"
+npm install
+
+echo "${green}Running gulp dist...${reset}"
+gulp dist
+
+echo "${green}Moving files to resource directory...${reset}"
+mkdir -p ${dir}/${target} && mv ${dir}/build ${dir}/${target}
+
+echo "${green}Vamp UI build: done.${reset}"
