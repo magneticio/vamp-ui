4da21fcda09736d285ee157c095f0e2b35333e7a
Rommert Zijlstra <rommert@thinkbright.nl>
2016-02-15 17:59:53 +0100

    added shim for ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,6 @@
+// Needed because https://github.com/angular/angular/issues/4902
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
 import { bootstrap } from 'angular2/platform/browser'
 import { Component } from 'angular2/core'
 import { HTTP_PROVIDERS } from 'angular2/http'
