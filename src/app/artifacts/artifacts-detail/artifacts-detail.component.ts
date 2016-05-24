f531bab010bac0ba77c328809d88fd53688c1889
Rommert Zijlstra <rommert@thinkbright.nl>
2016-05-24 11:19:23 +0200

    add rudimentary artifact-detail component

--- src/app/artifacts/artifacts-detail/artifacts-detail.component.ts
+++ src/app/artifacts/artifacts-detail/artifacts-detail.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { OnActivate , Router , ROUTER_DIRECTIVES , RouteSegment } from '@angular/router';
+
+import { ArtifactsService } from '../artifacts.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-artifacts-detail',
   templateUrl: 'artifacts-detail.component.html',
-  styleUrls: ['artifacts-detail.component.css']
+  styleUrls: ['artifacts-detail.component.css'],
+  directives: [ ROUTER_DIRECTIVES ]
 })
 export class ArtifactsDetailComponent implements OnInit {
 
-  constructor() {}
+  content = '';
+  resource;
+  selectedName;
+  selectedResource;
+
+  constructor(
+    private _artifacts : ArtifactsService,
+    private _router    : Router
+  ) {}
+
+  routerOnActivate( current: RouteSegment ) {
+    this.selectedResource = current.getParam( 'resource' );
+    this.selectedName     = decodeURIComponent( current.getParam( 'name' ) );
+
+    if ( this.selectedName && this.selectedName != 'undefined' )
+      this._artifacts[ this.selectedResource ].get( this.selectedName , { headers : { 'Accept' : 'application/x-yaml' } } )
+        .subscribe(
+          res => this.content = res
+        );
+  }
 
   ngOnInit() {
   }
