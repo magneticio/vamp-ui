702e58a982d744c394c827aefb65633342352e9f
Rommert Zijlstra <rommert@thinkbright.nl>
2016-05-24 17:34:14 +0200

    display all the info yaml

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {ApiService} from '../shared/api.service';
+import {NotificationsService} from '../shared/notifications.service';
 
 @Component({
   moduleId: module.id,
@@ -15,18 +16,38 @@ export class InfoComponent implements OnInit {
 
   protected _interval;
   protected _intervalTime = 30000; // 30s
+  protected _timeout;
 
   constructor(
-    private _api : ApiService
+    private _api : ApiService,
+    private _notifier : NotificationsService
   ) {
     this.endpoint = this._api._endpoint;
   }
 
   load() {
-    this.content = this._api.getAll( 'info' )
+    this.content = this._api.getAll( 'info' , { headers: { 'Accept' : 'application/x-yaml' } } )
       .subscribe(
-        res => this.content = res,
-        err => console.error( err )
+        res => {
+          this.content = res;
+
+          if ( this._notifier.get( 'connectionError' ) )
+            this._notifier.delete( 'connectionError' );
+
+          if ( !this._interval )
+            this.initPolling();
+        },
+        err => {
+          this._notifier.add( {
+            message : `Could not connect to the VAMP instance at ${ this.endpoint }`,
+            name    : 'connectionError',
+            type    : 'error'
+          } )
+
+          // Up the polling frequency when we can't connect to the API.
+          clearTimeout( this._timeout );
+          this._timeout = setTimeout( () => this.load() , this._intervalTime / 10 );
+        }
       );
   }
 
