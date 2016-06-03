2766dc65d45baedb9c7f5a1de92492efc41788b9
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-06-03 16:13:50 +0200

    Make read all table. Make table add data with polling. Add remove and edit buttons.

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('revampUi')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('revampUi', ['ngAnimate', 'ngTouch', 'ngMessages', 'ngAria', 'ui.router', 'toastr']);
+
+})();
