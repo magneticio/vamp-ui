533b4d67df0cf7e0526378247a1a07af39029968
Rommert Zijlstra <rommert@thinkbright.nl>
2016-06-15 12:05:16 +0200

    abstract some scss into shared styles

--- src/app/models/artifacts.js
+++ src/app/models/artifacts.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var endpoint        = window.location.origin + '/api/v1/',
+  var endpoint        = 'http://192.168.99.100:8080/api/v1/',
       defaultHeaders  = {
         'Content-Type': 'application/json'
       };
