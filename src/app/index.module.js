01f26b4ed24afa151cc3dacd38b59cb1d29cf3b6
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-05 10:27:57 +0200

    New start with open source template

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.footable', 'ui.codemirror', 'angular-ladda', 'ui.bootstrap', 'localytics.directives']);
-
-})();
