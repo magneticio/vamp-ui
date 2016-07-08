8dfef86fb3956cbd874ff8dbebfe70cbe275eef9
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-08 15:03:50 +0200

    Got page title working

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+angular
+  .module('app')
+  .config(routesConfig);
+
+/** @ngInject */
+function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('app', {
+      url: '/',
+      template: '<navbar></navbar><app></app>'
+    })
+    .state('readOneDeployment', {
+      url: '/deployment',
+      template: '<navbar></navbar><deployment></deployment>'
+    })
+    .state('readAllBlueprints', {
+      url: '/blueprints',
+      template: '<navbar></navbar><blueprints></blueprints>'
+    });
+}
