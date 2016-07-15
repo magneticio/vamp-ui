7ecc888580270b90701d86fcee277b3f9cb4ce9b
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-15 16:10:45 +0200

    Add table logic

--- src/app/readAllBlueprints/readAllBlueprints.js
+++ src/app/readAllBlueprints/readAllBlueprints.js
@@ -1,9 +1,19 @@
-function readAllBlueprintsController(Api) {
+function readAllBlueprintsController(Api, NgTableParams, $interval) {
   var self = this;
 
-  
+  function getData(params) {
+    return Api.readAll('blueprints').then(function (data) {
+      return data;
+    });
+  }
 
+  self.tableParams = new NgTableParams({}, {getData: getData});
 
+  function refresh() {
+    self.tableParams.reload()
+  }
+
+  $interval(refresh, 5000);
 }
 
 angular
