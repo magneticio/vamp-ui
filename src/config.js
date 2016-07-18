c615882c5198d108ca71474023adc0b012ae11ea
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-18 17:46:43 +0200

    Made popup work

---
+++ src/config.js
@@ -0,0 +1,16 @@
+angular
+  .module('app')
+  .config(pluginsConfig);
+
+function pluginsConfig(toastrConfig) {
+  //Configuration of the angular-toastr plugin. See: https://github.com/Foxandxss/angular-toastr
+  angular.extend(toastrConfig, {
+    allowHtml: false,
+    closeButton: true,
+    extendedTimeOut: 1000,
+    progressBar: true,
+    tapToDismiss: true,
+    positionClass: 'toast-top-right'
+  });
+
+}
