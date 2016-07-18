745ab7888ea7c6e4f9eb73af9e89b11683248481
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-07-18 17:46:43 +0200

    Made popup work

--- src/app/readAllBlueprints/readAllBlueprints.js
+++ src/app/readAllBlueprints/readAllBlueprints.js
@@ -1,5 +1,6 @@
-function readAllBlueprintsController(Api, NgTableParams, $interval) {
+function readAllBlueprintsController(Api, NgTableParams, $interval, $uibModal) {
   var self = this;
+  self.openDeleteModal = openDeleteModal;
 
   function getData(params) {
     return Api.readAll('blueprints').then(function (data) {
@@ -10,7 +11,23 @@ function readAllBlueprintsController(Api, NgTableParams, $interval) {
   self.tableParams = new NgTableParams({}, {getData: getData});
 
   function refresh() {
-    self.tableParams.reload()
+    self.tableParams.reload();
+  }
+
+  function openDeleteModal(blueprintId) {
+    var theBlueprintId = blueprintId;
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'app/deleteBlueprint/deleteBlueprint.html',
+      controller: 'deleteBlueprint',
+      size: 'sm',
+      resolve: {
+        blueprintId: function(){return theBlueprintId}
+      }
+    });
+
+
   }
 
   $interval(refresh, 5000);
