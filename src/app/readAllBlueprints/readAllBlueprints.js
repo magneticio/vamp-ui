d8cf46219ee1771ce3905bcdaa4b389644e1cfec
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-19 17:51:10 +0200

    Blueprints done

--- src/app/readAllBlueprints/readAllBlueprints.js
+++ src/app/readAllBlueprints/readAllBlueprints.js
@@ -1,17 +1,39 @@
-function readAllBlueprintsController(Api, NgTableParams, $interval, $uibModal) {
+function readAllBlueprintsController(Api, toastr, NgTableParams, $interval, $uibModal) {
   var self = this;
   self.openDeleteModal = openDeleteModal;
+  self.openDeployModal = openDeployModal;
+  
+  self.tableParams = new NgTableParams({}, {getData: getData});
 
-  function getData(params) {
+
+  function getData() {
     return Api.readAll('blueprints').then(function (data) {
       return data;
     });
   }
 
-  self.tableParams = new NgTableParams({}, {getData: getData});
 
   function refresh() {
     self.tableParams.reload();
+  }
+
+  function openDeployModal(blueprint) {
+    var theBlueprint = blueprint;
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'app/deployBlueprintModal/deployBlueprintModal.html',
+      controller: 'deployBlueprintModal',
+      size: 'sm',
+      resolve: {
+        blueprint: function(){return theBlueprint;},
+      }
+    });
+
+    modalInstance.result.then(function () {
+
+    });
+
   }
 
   function openDeleteModal(blueprintId) {
@@ -19,16 +41,30 @@ function readAllBlueprintsController(Api, NgTableParams, $interval, $uibModal) {
 
     var modalInstance = $uibModal.open({
       animation: true,
-      templateUrl: 'app/deleteBlueprint/deleteBlueprint.html',
-      controller: 'deleteBlueprint',
+      templateUrl: 'app/deleteResourceModal/deleteResourceModal.html',
+      controller: 'deleteResourceModal',
       size: 'sm',
       resolve: {
-        blueprintId: function(){return theBlueprintId}
+        id: function(){return theBlueprintId;},
+        title: function(){return 'Are you sure?';},
+        text: function(){return 'You are about to delete [' + theBlueprintId + ']. Confirm the deletion.';}
       }
     });
 
 
+    modalInstance.result.then(function (id) {
+      Api.delete('blueprints', id).then(blueprintDeleted, blueprintDeletedFailed);
+
+      function blueprintDeleted() {
+        toastr.success(id + ' has been deleted.', 'Blueprint deleted');
+      }
+
+      function blueprintDeletedFailed() {
+        toastr.error('Blueprint ' + id + ' could not be deleted', 'Blueprint not deleted');
+      }
+    });
   }
+
 
   $interval(refresh, 5000);
 }
