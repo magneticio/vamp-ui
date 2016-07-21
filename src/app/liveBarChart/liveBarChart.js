0111fd474a5af66452529e1f0b0bb5fe0758a891
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-21 17:11:32 +0200

    Implemented custom chart

---
+++ src/app/liveBarChart/liveBarChart.js
@@ -0,0 +1,63 @@
+function liveBarChartController($scope, $timeout, $timeout) {
+  var self = this;
+  self.bars = [];
+  console.log(self.base + ':newHealthValue');
+  self.innerPadding = 0;
+  self.overflowMargin = -self.barWidth - 8 + 'px';
+  self.paddingRight = 0;
+  self.animate = true;
+
+  function Bar(label, value) {
+
+    this.label = label;
+    this.value = value;
+    this.height = value * 100 + '%';
+    this.width = self.barWidth + 'px';
+  }
+
+  $scope.$on(self.base + ':newHealthValue', function(event, data) {
+    $timeout(function() {
+      self.animate = false;
+    }, 100);
+
+    $timeout(function() {
+      self.paddingRight = 0;
+      self.bars.push(new Bar(data.timestamp, data.value));
+    }, 200);
+
+
+
+    $timeout(function() {
+      self.animate = true;
+    }, 2000);
+
+    $timeout(function() {
+      self.paddingRight = self.barWidth + 4 + 'px';
+    }, 3000);
+
+
+
+
+
+    // self.innerPadding = 0;
+  });
+
+
+  // for(var i = 0; i < 24; i++) {
+  //   self.bars.push(new Bar('test', Math.random()));
+  // }
+
+
+}
+
+angular
+  .module('app')
+  .component('liveBarChart', {
+    templateUrl: 'app/liveBarChart/liveBarChart.html',
+    controller: liveBarChartController,
+    bindings: {
+      base: '@',
+      barWidth: '<'
+    }
+  });
+
