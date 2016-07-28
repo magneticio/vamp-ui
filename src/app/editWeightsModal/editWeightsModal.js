427c1f41c8b377fae07c8eda6d1d4ef5e40d6534
Bastiaan Niamut <bastiaan@magnetic.io>
2016-07-28 17:11:42 +0200

    Added health chart to deploy

--- src/app/editWeightsModal/editWeightsModal.js
+++ src/app/editWeightsModal/editWeightsModal.js
@@ -5,7 +5,38 @@ function editWeightsModal($scope, $uibModalInstance, weightValues) {
     ceil: 100
   }
 
+
+
   $scope.newWeightValues = angular.copy(weightValues);
+
+  function toArray(weightValues) {
+    var theArray = _.map(weightValues, function(value, prop) {
+      return {name: prop, value: value}
+    });
+
+    return theArray;
+  }
+  //This function seems to complicated. Might have to refactor in the future.
+  if(_.size($scope.newWeightValues) === 2) {
+    $scope.$watch('newWeightValues', function(newValue, oldValue) {
+      var newValueArray = toArray(newValue);
+      var oldValueArray = toArray(oldValue);
+
+      console.log(newValueArray);
+      console.log(oldValueArray);
+
+      if(newValueArray[0].value !== oldValueArray[0].value) {
+        console.log('first slider changed')
+        $scope.newWeightValues[newValueArray[1].name] = 100 - newValueArray[0].value;
+      }
+
+      if(newValueArray[1].value !== oldValueArray[1].value) {
+        console.log('second slider changed')
+        $scope.newWeightValues[newValueArray[0].name] = 100 - newValueArray[1].value;
+      }
+
+    }, true);
+  }
 
   $scope.ok = function () {
     $uibModalInstance.close($scope.newWeightValues);
