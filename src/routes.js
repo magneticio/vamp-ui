70110141f724c6c6d1a3e4d30fe3e3e7ff041f83
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-01 11:08:28 +0200

    Add hashtag to url

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ angular
 
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('!');
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
