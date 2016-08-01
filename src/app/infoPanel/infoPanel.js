283405cac4f671834633cf908dbef300dca1e44c
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-01 13:55:25 +0200

    Add info panel

---
+++ src/app/infoPanel/infoPanel.js
@@ -0,0 +1,40 @@
+function infoPanelController(Api, $rootScope) {
+  var self = this;
+
+  $rootScope.$watch('infoPanelActive', function(newValue) {
+    self.infoPanelActive = newValue;
+  });
+
+  Api.readAll('info').then(infoLoaded, infoLoadedFailed);
+  
+  function infoLoaded(data) {
+    console.log('INFO', data);
+
+    var info = {};
+
+    info.version = data.version;
+    info.persistence = data.persistence.database.type;
+    info.key_value_store = data.key_value.type;
+    info.gateway_driver = 'haproxy ' + data.gateway_driver.marshaller.haproxy;
+    info.container_driver = data.container_driver.type;
+    info.workflow_driver = '';
+
+    for (var name in data.workflow_driver) {
+      info["workflow_driver"] += info["workflow_driver"] === '' ? name : ', ' + name;
+    }
+
+    self.info = info;
+  }
+
+  function infoLoadedFailed() {
+
+  }
+}
+
+angular
+  .module('app')
+  .component('infoPanel', {
+    templateUrl: 'app/infoPanel/infoPanel.html',
+    controller: infoPanelController
+  });
+
