235d55847c3e14c1adde02fb51a5a6511d17f7de
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-08-02 20:29:00 +0200

    updating setEnvironment.sh

---
+++ setEnvironment.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+reset=`tput sgr0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+
+BASE_URL=$1
+
+if [ -z "$BASE_URL" ]; then
+  BASE_URL="window.location.origin"
+  ENDPOINT_URL="${BASE_URL} + '/api/v1/'"
+  echo "${green}Base URL not passed as an argument, using: ${yellow}${BASE_URL}${reset}"
+else
+  BASE_URL="http://${BASE_URL}"
+  ENDPOINT_URL="'${BASE_URL}/api/v1/'"
+  echo "${green}Base URL                                 : ${yellow}${BASE_URL}${reset}"
+fi
+
+echo "${green}Vamp API endpoint URL                    : ${yellow}${ENDPOINT_URL}${reset}"
+
+rm -rf ${dir}/src/app/environment 2> /dev/null
+mkdir -p ${dir}/src/app/environment && touch ${dir}/src/app/environment/environment.js
+
+echo "${green}Creating file                            : ${yellow}src/app/environment/environment.js${reset}"
+
+cat > ${dir}/src/app/environment/environment.js << EOF
+function Environment() {
+}
+
+Environment.prototype.getApiBaseUrl = function () {
+  return ${ENDPOINT_URL};
+};
+
+angular.module('app').service('Environment', Environment);
+EOF
+
+echo "${green}Done.${reset}"
