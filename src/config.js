82e6f5b3cd1603483627fc05863e42553a3d1688
Bastiaan Niamut <bastiaan@magnetic.io>
2016-08-11 14:46:19 +0200

    Was a toastr config error, took some time to find out what was happening. Fix #45

--- src/config.js
+++ src/config.js
@@ -2,18 +2,15 @@ angular
   .module('app')
   .config(pluginsConfig);
 
-function pluginsConfig(toastrConfig, ChartJsProvider) {
+function pluginsConfig(toastrConfig) {
   // Configuration of the angular-toastr plugin. See: https://github.com/Foxandxss/angular-toastr
   angular.extend(toastrConfig, {
     autoDismiss: false,
+    extendedTimeOut: 0,
     allowHtml: false,
     closeButton: true,
-    extendedTimeOut: 1000,
-    progressBar: false,
-    tapToDismiss: true,
+    tapToDismiss: false,
     positionClass: 'toast-top-right',
-    preventDuplicates: true
+    preventOpenDuplicates: false
   });
-
-  // ChartJsProvider
 }
