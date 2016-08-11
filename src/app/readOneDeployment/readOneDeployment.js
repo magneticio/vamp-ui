be59fa8eab504358750b62972e635005f30180d8
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-11 14:07:46 +0200

    Will not lose focus when polling. Fixes #48

--- src/app/readOneDeployment/readOneDeployment.js
+++ src/app/readOneDeployment/readOneDeployment.js
@@ -1,10 +1,8 @@
 /* global _*/
 function readOneDeploymentController(Api, $stateParams, $state, EventStreamHandler, $interval, $filter, DataManager) {
-  var noOfPoints = 250;
-
   var self = this;
-  var gatewaysResource = DataManager.resource('deployments');
-  self.data = gatewaysResource.readOne($stateParams.id);
+  var deploymentsResource = DataManager.resource('deployments');
+  self.data = deploymentsResource.readOne($stateParams.id);
   self.chart = {};
   self.currentHealth = undefined;
 
@@ -16,17 +14,34 @@ function readOneDeploymentController(Api, $stateParams, $state, EventStreamHandl
   self.saveNumberOfCpus = saveNumberOfCpus;
   self.saveSizeOfMemory = saveSizeOfMemory;
 
+  var polling = true;
+
+  refreshDeployment();
+  var intervalId = $interval(
+    function () {
+      if (polling) {
+        refreshDeployment();
+      }
+    },
+    3000
+  );
+
+  deploymentsResource.registerInterval(intervalId);
+
   function editNumberOfInstances(number) {
+    polling = false;
     self.editInstances = true;
     self.initialNumberOfInstances = angular.copy(number);
   }
 
   function editNumberOfCpus(number) {
+    polling = false;
     self.editCpu = true;
     self.initialNumberOfCpus = angular.copy(number);
   }
 
   function editSizeOfMemory(number) {
+    polling = false;
     self.editMemory = true;
     self.initialSizeOfMemory = angular.copy(number);
   }
@@ -34,46 +49,34 @@ function readOneDeploymentController(Api, $stateParams, $state, EventStreamHandl
   function saveNumberOfInstances(serviceScale, number) {
     self.editInstances = false;
     serviceScale.instances = number;
-    gatewaysResource.update($stateParams.id, self.data);
+    deploymentsResource.update($stateParams.id, self.data);
   }
 
   function saveNumberOfCpus(serviceScale, number) {
     self.editCpu = false;
     serviceScale.cpu = number;
-    gatewaysResource.update($stateParams.id, self.data);
+    deploymentsResource.update($stateParams.id, self.data);
   }
 
   function saveSizeOfMemory(serviceScale, number) {
     self.editMemory = false;
     serviceScale.memory = number;
-    gatewaysResource.update($stateParams.id, self.data);
+    deploymentsResource.update($stateParams.id, self.data);
   }
-
-  var deploymentId = $stateParams.id;
 
   function refreshDeployment() {
-    Api.read('deployments', deploymentId).then(deploymentLoaded, deploymentCouldNotBeLoaded);
+    Api.read('deployments', $stateParams.id).then(deploymentLoaded, deploymentCouldNotBeLoaded);
   }
-
 
   function deploymentLoaded(deployment) {
     self.data = deployment.data;
   }
 
-  refreshDeployment();
-  var intervalId = $interval(
-    function () {
-      refreshDeployment();
-    },
-    3000
-  );
-  gatewaysResource.registerInterval(intervalId);
-
   function deploymentCouldNotBeLoaded() {
     $state.go('readAllDeployments');
   }
 
-  EventStreamHandler.getStream('deployments:' + deploymentId, eventFired);
+  EventStreamHandler.getStream('deployments:' + $stateParams.id, eventFired);
 
   function eventFired(data) {
     if (_.includes(data.tags, 'health')) {
