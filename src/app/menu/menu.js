10486f6d23ec2677b6f878e1f7d453a66530fc76
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-12 17:16:32 +0200

    Memory stats work

--- src/app/menu/menu.js
+++ src/app/menu/menu.js
@@ -1,7 +1,8 @@
 /* global _*/
-function menuController($rootScope, $state) {
+function menuController($rootScope, $state, $interval, Api) {
   var self = this;
   self.toggleSettings = toggleSettings;
+  self.jvmData = {};
 
   var activeTable = {
     blueprints: [
@@ -73,6 +74,14 @@ function menuController($rootScope, $state) {
       goToState: 'readAllWorkflows'
     }
   ];
+
+  $interval(function() {
+    Api.readAll('info', {for: 'jvm'}).then(jvmDataLoaded);
+  }, 1000);
+  
+  function jvmDataLoaded(response) {
+    self.jvmData = response.data.jvm;
+  }
 }
 
 angular
