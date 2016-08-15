fb056c59c8681d4afac4f7ba66cf337a73801fbe
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-15 15:28:23 +0200

    Fix #39

--- src/config.js
+++ src/config.js
@@ -6,11 +6,12 @@ function pluginsConfig(toastrConfig) {
   // Configuration of the angular-toastr plugin. See: https://github.com/Foxandxss/angular-toastr
   angular.extend(toastrConfig, {
     autoDismiss: false,
+    timeOut: 0,
     extendedTimeOut: 0,
     allowHtml: false,
     closeButton: true,
     tapToDismiss: false,
     positionClass: 'toast-top-right',
-    preventOpenDuplicates: false
+    preventOpenDuplicates: true
   });
 }
