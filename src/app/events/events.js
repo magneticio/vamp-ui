c6c6e96065259ac0973328ab741325571597986a
Bastiaan Niamut <bastiaan@bleeve.nl>
2016-08-15 12:35:56 +0200

    Fix #37

---
+++ src/app/events/events.js
@@ -0,0 +1,42 @@
+function Events(EventStreamHandler, $interval) {
+  var self = this;
+  self.events = [];
+  self.setEventsUpdated = setEventsUpdated;
+  self.start = start;
+  var eventsUpdated = undefined;
+
+  var eventCache = [];
+
+  EventStreamHandler.getStream(undefined, eventFired);
+
+  function eventFired(data) {
+    eventCache.push(data);
+  }
+
+  function cacheToScope() {
+    var tempEvents = self.events.concat(eventCache);
+    while (tempEvents.length > 25) {
+      tempEvents.shift();
+    }
+    self.events = tempEvents;
+    eventCache = [];
+    console.log()
+    eventsUpdated(self.events);
+  }
+
+  function setEventsUpdated(eventsUpdatedFunction) {
+    eventsUpdated = eventsUpdatedFunction;
+    cacheToScope();
+  }
+
+  function start() {
+    $interval(function () {
+      cacheToScope()
+    }, 1000);
+  }
+}
+
+angular
+  .module('app')
+  .service('Events', Events);
+
