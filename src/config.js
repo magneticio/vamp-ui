69d68cac2084f933037c61aa9c0c54f9d17faa58
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-08-29 12:25:51 +0200

    adding timeout to toastr messages

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ angular
 function pluginsConfig(toastrConfig) {
   // Configuration of the angular-toastr plugin. See: https://github.com/Foxandxss/angular-toastr
   angular.extend(toastrConfig, {
-    autoDismiss: false,
-    timeOut: 0,
+    autoDismiss: true,
+    timeOut: 5000,
     extendedTimeOut: 0,
     allowHtml: false,
     closeButton: true,
-    tapToDismiss: false,
+    tapToDismiss: true,
     positionClass: 'toast-top-right',
     preventOpenDuplicates: true
   });
