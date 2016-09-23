1b605cf44f71732435f21978198cefb43d595a51
Mikael Sörlin <mikael@magnetic.io>
2016-09-23 17:46:13 +0200

    Update asset URI's to use relative paths

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -10,7 +10,7 @@ yellow=`tput setaf 3`
 BASE_URL=$1
 
 if [ -z "$BASE_URL" ]; then
-  BASE_URL="window.location.origin"
+  BASE_URL="window.location.origin + window.location.pathname"
   ENDPOINT_URL="${BASE_URL} + '/api/v1/'"
   MIXPANEL_API_KEY="3dc73f826819c8e11d0d9898ca4291c8"
   echo "${green}Base URL not passed as an argument, using: ${yellow}${BASE_URL}${reset}"
