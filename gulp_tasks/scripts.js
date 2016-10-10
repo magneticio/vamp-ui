a9fd0904ef5105bbd4152fbca94cae075f1ce2ff
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-10-10 14:36:00 +0200

    fail build on error

--- gulp_tasks/scripts.js
+++ gulp_tasks/scripts.js
@@ -3,12 +3,16 @@ const eslint = require('gulp-eslint');
 
 const conf = require('../conf/gulp.conf');
 
-gulp.task('scripts', scripts);
-
-function scripts() {
+gulp.task('scripts', function () {
   return gulp.src(conf.path.src('**/*.js'))
     .pipe(eslint())
     .pipe(eslint.format())
-
     .pipe(gulp.dest(conf.path.tmp()));
-}
+});
+
+gulp.task('failAfterError', function () {
+  return gulp.src(conf.path.src('**/*.js'))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 gulp.registry(hub);
 
 gulp.task('inject', gulp.series(gulp.parallel('styles', 'scripts'), 'inject'));
-gulp.task('build', gulp.series('partials', gulp.parallel('inject', 'other'), 'build'));
+gulp.task('build', gulp.series('partials', gulp.parallel('inject', 'other', 'failAfterError'), 'build'));
 gulp.task('test', gulp.series('scripts', 'karma:single-run'));
 gulp.task('test:auto', gulp.series('watch', 'karma:auto-run'));
 gulp.task('serve', gulp.series('inject', 'watch', 'browsersync'));
