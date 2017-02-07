7ba829b16a9f875e1de91f7dafde8e478882a7a8
eyal-magnetic <eyal@magnetic.io>
2017-02-07 11:46:13 +0100

    Style changes
    - Got rid of useless scrollbars
    - New scroller styles for Safari, Chrome and IE
    - Editor stretch to bottom

--- src/app/crud/artifact.js
+++ src/app/crud/artifact.js
@@ -19,38 +19,12 @@ function ArtifactService($vamp) {
     theme: 'vamp',
     mode: 'yaml',
     firstLineNumber: 1,
-    maxLines: Infinity,
     useSoftTabs: true,
     advanced: {
-      tabSize: 2
+      tabSize: 2,
+      maxLines: Infinity
     },
-    onLoad: function (editor) {
-      editor.focus();
-      editor.$blockScrolling = 'Infinity';
-
-      var heightUpdateFunction = function () {
-          // http://stackoverflow.com/questions/11584061/
-        var minHeight = 40 * editor.renderer.lineHeight;
-        var newHeight =
-                    editor.getSession().getScreenLength() *
-                    editor.renderer.lineHeight +
-                    editor.renderer.scrollBar.getWidth();
-
-        newHeight = newHeight < minHeight ? minHeight : newHeight;
-        $('#editor').height(newHeight.toString() + "px");
-        $('#editor-section').height(newHeight.toString() + "px");
-
-          // This call is required for the editor to fix all of
-          // its inner structure for adapting to a change in size
-        editor.resize();
-      };
-
-      // Set initial size to match initial content
-      heightUpdateFunction();
-
-      // Whenever a change happens inside the ACE editor, update
-      // the size again
-      editor.getSession().on('change', heightUpdateFunction);
+    onLoad: function () {
     }
   };
 
