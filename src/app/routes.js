d2de76c4588d6d00605c7ace64d75ad4ff2acf2e
eyal-magnetic <eyal@magnetic.io>
2017-03-29 11:56:25 +0200

    Single root state

--- src/app/routes.js
+++ src/app/routes.js
@@ -20,23 +20,21 @@ function routesConfig($stateProvider, $urlRouterProvider) {
 
   if (!eeRouting) {
     $urlRouterProvider.otherwise('/vamp/deployments');
-
-    $stateProvider
-      .state('vamp', {
-        url: '/vamp',
-        abstract: true,
-        views: {
-          app: {
-            templateUrl: 'app/home/templates/home.html'
-          }
-        },
-        ncyBreadcrumb: {
-          skip: true
-        }
-      });
   }
 
   $stateProvider
+    .state('vamp', {
+      url: '/vamp',
+      abstract: true,
+      views: {
+        app: {
+          templateUrl: 'app/home/templates/home.html'
+        }
+      },
+      ncyBreadcrumb: {
+        skip: true
+      }
+    })
     .state('artifacts', {
       parent: eeRouting && eeRouting.rootName || 'vamp',
       url: '/:kind?page&searchTerm',
