6404f424741ffa7dd3569810cc0c5d31a670d19a
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2018-04-13 17:15:50 +0200

    condition and deployment pagination

--- src/app/conditions/controllers/conditionsController.js
+++ src/app/conditions/controllers/conditionsController.js
@@ -1,9 +1,10 @@
-function conditionsController($scope, $state, $stateParams, artifactsMetadata, $vamp) {
+function conditionsController($controller, $scope, $state, $stateParams, artifactsMetadata, $vamp) {
   var $ctrl = this;
+  $controller('PaginationController', {$ctrl: $ctrl, $vamp: $vamp, $scope: $scope, artifactsMetadata: artifactsMetadata, $stateParams: $stateParams});
 
   $ctrl.conditions = [];
   $ctrl.config = artifactsMetadata;
-  var path = '/conditions';
+  $ctrl.path = '/conditions';
 
   $ctrl.add = function () {
     $state.go('.add');
@@ -16,22 +17,23 @@ function conditionsController($scope, $state, $stateParams, artifactsMetadata, $
   };
 
   $ctrl.delete = function (condition) {
-    return $vamp.delete(path + '/' + condition.name, angular.toJson(condition));
+    return $vamp.delete($ctrl.path + '/' + condition.name, angular.toJson(condition));
   };
 
-  $scope.$on(path, function (e, response) {
+  $scope.$on($ctrl.path, function (e, response) {
     $scope.artifactsLoaded = true;
+    $ctrl.parseHeaders(response);
     angular.copy(_.orderBy(response.data, 'name'), $ctrl.conditions);
   });
 
   $scope.$on('/events/stream', function (e, response) {
     if (_.includes(response.data.tags, 'conditions')) {
-      $vamp.emit(path);
+      $ctrl.refresh();
     }
   });
 
-  $vamp.emit(path);
+  $ctrl.refresh();
 }
 
-conditionsController.$inject = ['$scope', '$state', '$stateParams', 'artifactsMetadata', '$vamp'];
+conditionsController.$inject = ['$controller', '$scope', '$state', '$stateParams', 'artifactsMetadata', '$vamp'];
 angular.module('vamp-ui').controller('conditionsController', conditionsController);
--- src/app/deployments/controllers/deploymentsController.js
+++ src/app/deployments/controllers/deploymentsController.js
@@ -1,10 +1,11 @@
-function deploymentsController($scope, $state, $stateParams, artifactsMetadata, $vamp, $vampDeployment) {
+function deploymentsController($controller, $scope, $state, $stateParams, artifactsMetadata, $vamp, $vampDeployment) {
   var $ctrl = this;
+  $controller('PaginationController', {$ctrl: $ctrl, $vamp: $vamp, $scope: $scope, artifactsMetadata: artifactsMetadata, $stateParams: $stateParams});
 
   var _deployments = [];
   $ctrl.deployments = [];
   $ctrl.config = artifactsMetadata;
-  var path = '/deployments';
+  $ctrl.path = '/deployments';
 
   $ctrl.add = function () {
     $state.go('.add');
@@ -20,11 +21,12 @@ function deploymentsController($scope, $state, $stateParams, artifactsMetadata, 
     var index = _.findIndex(_deployments, {
       name: deployment.name
     });
-    return $vamp.delete(path + '/' + _deployments[index].name, angular.toJson(_deployments[index]));
+    return $vamp.delete($ctrl.path + '/' + _deployments[index].name, angular.toJson(_deployments[index]));
   };
 
-  $scope.$on(path, function (e, response) {
+  $scope.$on($ctrl.path, function (e, response) {
     $scope.artifactsLoaded = true;
+    $ctrl.parseHeaders(response);
     angular.copy(_.orderBy(response.data, 'name'), _deployments);
     angular.copy(_deployments, $ctrl.deployments);
     angular.forEach($ctrl.deployments, function (ar) {
@@ -38,11 +40,11 @@ function deploymentsController($scope, $state, $stateParams, artifactsMetadata, 
       _.includes(response.data.tags, 'deployed') ||
       _.includes(response.data.tags, 'undeployed') ||
       _.includes(response.data.tags, 'synchronization')) || _.includes(response.data.tags, 'deployments')) {
-      $vamp.emit(path);
+      $ctrl.refresh();
     }
   });
 
-  $vamp.emit(path);
+  $ctrl.refresh();
 
   function getScale(deployment) {
     return $vampDeployment.scale(deployment);
@@ -53,5 +55,5 @@ function deploymentsController($scope, $state, $stateParams, artifactsMetadata, 
   }
 }
 
-deploymentsController.$inject = ['$scope', '$state', '$stateParams', 'artifactsMetadata', '$vamp', '$vampDeployment'];
+deploymentsController.$inject = ['$controller', '$scope', '$state', '$stateParams', 'artifactsMetadata', '$vamp', '$vampDeployment'];
 angular.module('vamp-ui').controller('deploymentsController', deploymentsController);
